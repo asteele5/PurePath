38f331dabe879a07523fac0e3beb01a97383ca6b
Alexander Steele <steele.j.alexander@gmail.com>
2023-02-12 02:18:20 -0500

    Get nearby restaurants from google api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import axios from 'axios';
+import { isFulfilled } from 'q';
 
 const containerStyle = {
   width: '400px',
@@ -9,18 +11,13 @@ const containerStyle = {
 };
 
 
-function Button() {
-  const [name, setName] = useState("Click Me");
 
-  function handleClick() {
-    console.log("button clicked");
+function sustainabilityScore({restaurant, walking, vegetarian}) {
 
-    setName("Thank you!");
+  return walking + vegetarian;
 
-  }
-
-  return <button onClick={handleClick}> {name} </button>;
 }
+
 
 
 
@@ -28,18 +25,128 @@ function App() {
 
   const [lat, setLat] = useState(0);
   const [long, setLong] = useState(0);
+  const [nearbyRestaurants, setNearbyRestaurants] = useState(null);
+  const [nearbyWalkingRestaurants, setNearbyWalkingRestaurants] = useState(null);
+
+  function Button({nearbyRestaurants, lat, long}) {
+
+    let commuteTime = 30; //change based on user input
+
+    let walkingNum = 87; //ask rohit
+
+    let drivingNum = 804; //ask rohit
+
+    
+
+    let openNow = "false";
+
+    let walking = true;
+  
+
+    function handleClick() {
+  
+      
+      console.log("button clicked");
+
+      let radius = commuteTime*drivingNum;
+
+        axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location="+lat+"%2C"+long+"&radius="+radius+"&type=restaurant&opennow="+openNow+"&key=AIzaSyD_Mx8GV07MegosFOSpjyOjuQUu5QbV5Mw").then((response) => {
+          setNearbyRestaurants(response.data);
+        });
+
+        // let next_page_token = nearbyRestaurants.next_page_token;
+
+        // axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location=&next_page_token="+next_page_token+"&key=AIzaSyD_Mx8GV07MegosFOSpjyOjuQUu5QbV5Mw").then((response) => {
+        //   setNearbyRestaurants(response.data);
+        // }); 
+
+        if(nearbyRestaurants != null){
+          for(let i = 0; i < nearbyRestaurants.results.length; i++) {
+            nearbyRestaurants.results[i]["walking"] = 0;
+            // console.log("outside loop");
+            console.log(nearbyRestaurants.results[i]);
+          }
+        }
+
+      if (walking == true) {
+
+        let radius = commuteTime*walkingNum;
+        
+        axios.get("https://maps.googleapis.com/maps/api/place/nearbysearch/json?&location="+lat+"%2C"+long+"&radius="+radius+"&type=restaurant&opennow="+openNow+"&key=AIzaSyD_Mx8GV07MegosFOSpjyOjuQUu5QbV5Mw").then((response) => {
+          setNearbyWalkingRestaurants(response.data);
+        });
+        if(nearbyWalkingRestaurants != null){
+          // console.log(nearbyRestaurants.results[0]);
+          // console.log(nearbyRestaurants.next_page_token);
+          for(let i = 0; i < nearbyWalkingRestaurants.results.length; i++) {
+            
+            if(nearbyRestaurants != null && nearbyRestaurants.results[i]['name'] === nearbyWalkingRestaurants.results[i]['name']) {
+              console.log("inside loop");  
+              nearbyRestaurants.results[i]["walking"] = 1;
+                console.log(nearbyRestaurants.results[i]);
+            }
+            // console.log(nearbyRestaurants.results[i]);
+          }
+        }
+          
+      }
+
+      
+
+      }
+
+      
+      
+      
+      return <button onClick={handleClick}> Submit </button>;
+  
+  
+    }
+  
+    
+  
+
+  
+  
+  
+
+
+    
 
     navigator.geolocation.getCurrentPosition(function(position) {
       setLat(position.coords.latitude);
       setLong(position.coords.longitude);
-      console.log("Latitude is :", lat);
-      console.log("Longitude is :", long);
+      // console.log("Latitude is :", lat);
+      // console.log("Longitude is :", long);
     });
 
     const center = {
       lat: lat,
       lng: long
     };
+
+    // const getAddressURL = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+lat+","+long+"&key=AIzaSyD_Mx8GV07MegosFOSpjyOjuQUu5QbV5Mw";
+
+    // axios.get(getAddressURL).then((response) => {
+    //   setCurrentAddress(response.data);
+    // });
+
+    // console.log(currentAddress);
+
+    
+
+    
+
+    
+      // console.log(nextPageToken);
+      // nearbyRestaurants.forEach(element => console.log(element));
+
+  
+
+  
+    
+    // const restaurants = JSON.parse(nearbyRestaurants);
+    // console.log(restaurants);
 
 
   return (
@@ -58,7 +165,7 @@ function App() {
           Learn React
         </a>
         {/* <button onClick={getLocation}>Get Location </button> */}
-        <Button />
+        <Button nearbyRestaurants={nearbyRestaurants} lat={lat} long={long}/>
       {/* </header> */} 
       <LoadScript
         googleMapsApiKey="AIzaSyD_Mx8GV07MegosFOSpjyOjuQUu5QbV5Mw"
