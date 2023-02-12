ee921cc1bcf9c86df70db4a1a04e7cd0dd1aaeb7
Caroline Kerr <123588148+cjkerr128@users.noreply.github.com>
2023-02-12 00:18:14 -0500

    Add files via upload

---
+++ input.js
@@ -0,0 +1,21 @@
+var walk == false;
+
+function check()
+  {
+      if(document.getElementById("walk").checked) {
+    walk = true;
+  }
+
+var select;
+window.onload = function () {
+    select = document.getElementById('dropdown');
+    console.log(select);
+}
+
+function changeHiddenInput(objDropDown) {
+    console.log(objDropDown);
+    var objHidden = document.getElementById("hiddenInput");
+    objHidden.value = objDropDown.value;
+    var commuteTime = objHidden.value;
+    result.innerHTML = a || "";
+}
